8210ec060d3f0fa24e99a62beecdbcc18c07e179
CrashXBETAX <gabrielhagui@gmail.com>
2023-11-18 00:28:51 -0300

    Incluindo os exemplos

--- IniciarProjetoParaLinuxEMacOS.sh
+++ IniciarProjetoParaLinuxEMacOS.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip install Pillow selenium bs4
 python3 Processamento_Python.py
 echo "O resultado foi salvo na pasta 'Resultado'. Por favor, abra um arquivo HTML correspondente ao ano e à linha que você selecionou anteriormente."
 read -p "Pressione [Enter] para continuar..."
